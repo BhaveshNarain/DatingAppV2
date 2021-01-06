acb597db897117a069eb52254ff6a3b85e57cb6a
HICNET\BhaveshN <bhaveshn@hollard.co.za>
2021-01-06 18:11:35 +0200

    End of Section 13

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -44,7 +44,8 @@ namespace API.Controllers
             {
                 username = user.UserName,
                 Token = _tokenService.CreateToken(user),
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
 
             };
         }
@@ -73,7 +74,8 @@ namespace API.Controllers
                 username = user.UserName,
                 Token = _tokenService.CreateToken(user),
                 PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url,
-                KnownAs = user.KnownAs
+                KnownAs = user.KnownAs,
+                Gender = user.Gender
             };
         }
         private async Task<bool> UserExists(string username)
